f7df61713f05b919285aec727cd1304dba041402
George <georgerubaylo@gmail.com>
2024-05-01 16:50:18 +0300

    Tried animation v2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,6 @@ export default function Home() {
   return (
     <main className="h-full text-4xl text-center bg-white">
       <Scene />
-      <h1 className="h-full m-40 text-4xl text-cente absolute text-white">HELLO</h1>
     </main>
   )
 }
